7b91f4423b3138020c739a4c21db9893700dc1bc
Anna Beatriz Garcia <annagarcia@id.uff.br>
2022-04-01 14:24:28 -0300

    Adicionando Requisito 4 e 5(parcial)

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,17 @@
 export const LOGIN = 'LOGIN';
+export const GET_CURRENCIES = 'GET_CURRENCIES';
 
 export const submitLogin = (user) => ({ type: LOGIN, user });// Coloque aqui suas actions
+
+export const getCurrencies = (currencies) => ({ type: GET_CURRENCIES, currencies });
+
+export const fetchAPI = () => async (dispatch) => {
+  try {
+    const response = await fetch('https://economia.awesomeapi.com.br/json/all');
+    const data = await response.json();
+    const objectKeys = Object.keys(data).filter((item) => item !== 'USDT');
+    return dispatch(getCurrencies(objectKeys));
+  } catch (error) {
+    console.log(error.message);
+  }
+};
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,15 +1,71 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import Header from '../components/Header';
+import { fetchAPI } from '../actions';
 
 class Wallet extends React.Component {
+  componentDidMount() {
+    const { fetchAPIaction } = this.props;
+    fetchAPIaction();
+  }
+
   render() {
+    /*     const { currencies } = this.props; */
     return (
       <div>
         <h1>TrybeWallet</h1>
         <Header />
+        <form>
+          <label htmlFor="value">
+            Valor:
+            <input type="text" id="value" data-testid="value-input" />
+          </label>
+          <label htmlFor="description">
+            Descrição:
+            <input type="text" id="description" data-testid="description-input" />
+          </label>
+          <label htmlFor="currency">
+            Moeda:
+            <select id="currency" data-testid="currency-input">
+              <option>BRL</option>
+            </select>
+          </label>
+          <label htmlFor="payment">
+            Método de pagamento:
+            <select id="payment" data-testid="method-input">
+              <option>Dinheiro</option>
+              <option>Cartão de crédito</option>
+              <option>Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="tag">
+            Tag:
+            <select id="tag" data-testid="tag-input">
+              <option>Alimentação</option>
+              <option>Lazer</option>
+              <option>Trabalho</option>
+              <option>Transporte</option>
+              <option>Saúde</option>
+            </select>
+          </label>
+        </form>
+
       </div>
     );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  currencies: state.currencies,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchAPIaction: () => dispatch(fetchAPI()),
+});
+
+Wallet.propTypes = {
+  fetchAPIaction: PropTypes.func.isRequired,
+/*   currencies: PropTypes.arrayOf(PropTypes.string).isRequired, */
+};
+export default connect(mapStateToProps, mapDispatchToProps)(Wallet);
